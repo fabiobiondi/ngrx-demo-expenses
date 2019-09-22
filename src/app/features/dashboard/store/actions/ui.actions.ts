5be8faa7c7d79e0e7708633c118f777fe2751fd1
Fabio Biondi <info@fabiobiondi.com>
2019-09-22 15:43:16 +0200

    Add UI actions

--- src/app/features/dashboard/store/actions/ui.actions.ts
+++ src/app/features/dashboard/store/actions/ui.actions.ts
@@ -1,7 +1,6 @@
 import { createAction, props } from '@ngrx/store';
 import { UiFilterState } from '../reducers/ui.reducer';
 
-/*
 export const openFilterPanel = createAction(
   '[ui] Open Filter Panel',
 );
@@ -14,4 +13,3 @@ export const setFilter = createAction(
   '[ui] Set Filter',
   props<{ filter: UiFilterState }>()
 );
-*/
