d4e58013f8c1ca2e7947fc94b4f79b8b0bb01643
Fabio Biondi <info@fabiobiondi.com>
2019-09-23 02:01:01 +0200

    Remove required validator on select

--- src/app/features/dashboard/components/items-form.component.ts
+++ src/app/features/dashboard/components/items-form.component.ts
@@ -12,7 +12,7 @@ import { NgForm } from '@angular/forms';
       <input placeholder="Expense Description" type="text" name="text" [ngModel] class="form-control" required>
       <input placeholder="Amount ($)" type="number" name="amount" [ngModel] class="form-control" required>
 
-      <select [ngModel] name="category" class="form-control" required>
+      <select [ngModel] name="category" class="form-control">
         <option [value]="null">Select Category</option>
         <option *ngFor="let category of categories">{{category}}</option>
       </select>
