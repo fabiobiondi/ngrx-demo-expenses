b2d00b665b775f6183b865095cf3a25d9308c7b6
Fabio Biondi <info@fabiobiondi.com>
2019-09-25 17:38:51 +0200

    Add hidden content

--- src/app/features/dashboard/store/reducers/items.reducer.ts
+++ src/app/features/dashboard/store/reducers/items.reducer.ts
@@ -3,9 +3,64 @@ import { Item } from '../../../../model/item';
 import {
 
 } from '../actions/items.actions';
+import { loadItemsSuccess } from '../actions/items.actions';
+import { deleteItemSuccess } from '../actions/items.actions';
+import { addItemSuccess } from '../actions/items.actions';
 
 const initialState: Item[] = [];
 
 export const itemsReducer = createReducer(
   initialState,
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+on(loadItemsSuccess, (state, action) => [...action.items ]),
+on(deleteItemSuccess, (state, action) => state.filter(p => p.id !== action.id)),
+on(addItemSuccess, (state, action) => [...state, { ...action.item }]),
+*/
