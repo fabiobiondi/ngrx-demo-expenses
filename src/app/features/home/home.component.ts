e98d11de809af6f48c5fcdea02b9e72c7d684658
Fabio Biondi <info@fabiobiondi.com>
2019-10-12 04:43:21 +0200

    Add Home feature

---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fb-home',
+  template: `
+    <p>
+      home works!
+    </p>
+  `,
+  styles: []
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
