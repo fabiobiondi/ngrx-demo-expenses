a4b784bb5b4f0ac4adf48eb9fb3213e6983dd9ae
Fabio Biondi <info@fabiobiondi.com>
2019-10-12 04:47:38 +0200

    Add Home logo

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,19 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'fb-home',
   template: `
-    <p>
-      home works!
-    </p>
+    <div class="text-center mt-5">
+      <img src="assets/ngrx.svg" width="200">
+      <h1>NGRX DEMO</h1>
+    </div>
   `,
   styles: []
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class HomeComponent {}
